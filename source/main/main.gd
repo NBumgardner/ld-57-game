ef9eb1d19c0925a4325505a3eded9d935cccc3f8
Harmoen <harmon.the.emu@gmail.com>
2025-04-05 17:28:02 -0500

    Updated Game Icon

--- source/main/main.gd
+++ source/main/main.gd
@@ -5,8 +5,6 @@ const levels : Array = preload("res://source/levels/game_levels.tres").levels
 @export var current_level : Level
 var current_level_id : int = 0
 
-@onready var background_floor: Polygon2D = $Background/Floor
-@onready var background_floor_side: Polygon2D = $Background/Floor/FloorSide
 @onready var game_camera: Camera2D = $GameCamera
 
 @onready var player: Player = $Player
@@ -35,28 +33,28 @@ func _input(event: InputEvent) -> void:
 		else:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 
-	if event.is_action_pressed("debug_skip_down"):
-		print_debug("Debug skip to next lower floor.")
-		transition_to_floor_next_lower()
+	#if event.is_action_pressed("debug_skip_down"):
+		#print_debug("Debug skip to next lower floor.")
+		#transition_to_floor_next_lower()
 
 
-func transition_to_floor_next_lower():
-	background_floor.visible = false
-	background_floor_side.visible = false
-
-	if game_camera_next_floor_distance_y < 1:
-		print_debug("Keep camera still since vector y movement would be less than 1.")
-		return
-
-	game_camera.set_position(
-		game_camera.get_position()
-		+ Vector2(0, game_camera_next_floor_distance_y)
-	)
-	game_camera_next_floor_distance_y = (
-		game_camera_next_floor_distance_y
-		/ game_camera_next_floor_distance_divisor_y
-	)
-
+#func transition_to_floor_next_lower():
+	#background_floor.visible = false
+	#background_floor_side.visible = false
+#
+	#if game_camera_next_floor_distance_y < 1:
+		#print_debug("Keep camera still since vector y movement would be less than 1.")
+		#return
+#
+	#game_camera.set_position(
+		#game_camera.get_position()
+		#+ Vector2(0, game_camera_next_floor_distance_y)
+	#)
+	#game_camera_next_floor_distance_y = (
+		#game_camera_next_floor_distance_y
+		#/ game_camera_next_floor_distance_divisor_y
+	#)
+#
 
 func load_next_level() -> void:
 	screen_wipe.wipe_start()
