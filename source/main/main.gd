f551827f80ad163ed8027835bd3307f6d1b875c0
Noah Bumgardner <noah@playgamecafe.com>
2025-04-05 08:08:26 -0500

    Move Camera Down Up to 7 Times

--- source/main/main.gd
+++ source/main/main.gd
@@ -3,12 +3,17 @@ extends Node
 
 @onready var background_floor: Polygon2D = $Background/Floor
 @onready var background_floor_side: Polygon2D = $Background/FloorSide
+@onready var game_camera: Camera2D = $GameCamera
 
 
 var fullscreen : bool = false
+var game_camera_next_floor_distance_initial_y = 120
+var game_camera_next_floor_distance_y : float
+var game_camera_next_floor_distance_divisor_y = 2
 
 
 func _ready() -> void:
+	game_camera_next_floor_distance_y = game_camera_next_floor_distance_initial_y
 	pass
 
 
@@ -28,3 +33,16 @@ func _input(event: InputEvent) -> void:
 func transition_to_floor_next_lower():
 	background_floor.visible = false
 	background_floor_side.visible = false
+
+	if game_camera_next_floor_distance_y < 1:
+		print_debug("Keep camera still since vector y movement would be less than 1.")
+		return
+
+	game_camera.set_position(
+		game_camera.get_position()
+		+ Vector2(0, game_camera_next_floor_distance_y)
+	)
+	game_camera_next_floor_distance_y = (
+		game_camera_next_floor_distance_y
+		/ game_camera_next_floor_distance_divisor_y
+	)
