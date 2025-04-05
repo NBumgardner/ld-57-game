63b37d2767c856f427adae7c8b1b61c523c58b3d
Noah Bumgardner <noah@playgamecafe.com>
2025-04-05 08:41:39 -0500

    Listen to All Enter Events, Ignored

--- source/ladder/ladder.gd
+++ source/ladder/ladder.gd
@@ -11,3 +11,15 @@ func _process(_elta: float) -> void:
 
 func _on_body_entered(body):
 	print_debug("Ladder overlap detected for body:", body)
+
+
+func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	print_debug("Ladder overlap detected for body shape:", body_rid, body, body_shape_index, local_shape_index)
+
+
+func _on_area_entered(area):
+	print_debug("Ladder overlap detected for area:", area)
+
+
+func _on_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
+	print_debug("Ladder overlap detected for area shape:", area_rid, area, area_shape_index, local_shape_index)
