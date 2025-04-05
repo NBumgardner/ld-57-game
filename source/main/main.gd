64c36eb9e994e2477066bd0e6c4958bb0cc66d14
Harmoen <harmon.the.emu@gmail.com>
2025-04-05 01:24:02 -0500

    Basic Player Controller

--- source/main/main.gd
+++ source/main/main.gd
@@ -1,5 +1,16 @@
 extends Node
 
+var fullscreen : bool = false
+
 
 func _ready() -> void:
 	pass
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("fullscreen_toggle"):
+		fullscreen = !fullscreen
+		if fullscreen:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
---
+++ source/player/player.gd
@@ -0,0 +1,24 @@
+class_name Player
+extends CharacterBody2D
+
+var input_direction : Vector2 = Vector2.ZERO
+@export var movement_speed : float = 250
+
+
+func _ready() -> void:
+	pass
+
+
+func _process(_elta: float) -> void:
+	pass
+
+
+func _physics_process(_delta: float) -> void:
+	velocity = input_direction * movement_speed
+	
+	move_and_slide()
+
+
+
+func _input(_event: InputEvent) -> void:
+	input_direction = Input.get_vector("left","right","up","down").normalized()
