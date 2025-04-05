c083e37daea9c19b60b8cca752120606fd79b9e9
Noah Bumgardner <noah@playgamecafe.com>
2025-04-05 08:48:25 -0500

    Cut Signals from Ladder

--- source/ladder/ladder.gd
+++ source/ladder/ladder.gd
@@ -7,19 +7,3 @@ func _ready() -> void:
 
 func _process(_elta: float) -> void:
 	pass
-
-
-func _on_body_entered(body):
-	print_debug("Ladder overlap detected for body:", body)
-
-
-func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
-	print_debug("Ladder overlap detected for body shape:", body_rid, body, body_shape_index, local_shape_index)
-
-
-func _on_area_entered(area):
-	print_debug("Ladder overlap detected for area:", area)
-
-
-func _on_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
-	print_debug("Ladder overlap detected for area shape:", area_rid, area, area_shape_index, local_shape_index)
