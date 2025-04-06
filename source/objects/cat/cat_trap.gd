2a55f5836896db1b13d4f5d37d36305e07a80024
Harmoen <harmon.the.emu@gmail.com>
2025-04-06 13:30:44 -0500

    Updated Cat Trap

--- source/objects/cat/cat_trap.gd
+++ source/objects/cat/cat_trap.gd
@@ -5,11 +5,12 @@ extends Area2D
 @onready var spot_light: Sprite2D = $SpotLight
 @onready var animation: AnimationPlayer = $AnimationPlayer
 
+@export var trap_ready : bool = true
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered_area)
 
 
 func _on_body_entered_area(body: Node2D) -> void:
-	if body is Player:
+	if trap_ready and body is Player:
 		animation.play("drop_hand")
