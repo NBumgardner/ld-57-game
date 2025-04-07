6212dd8840d3cbad9dda6b2696de9fc549b79a91
Harmoen <harmon.the.emu@gmail.com>
2025-04-07 05:35:53 -0500

    Added Audio Sliders

--- source/globals/audio_manager.gd
+++ source/globals/audio_manager.gd
@@ -2,10 +2,19 @@ extends Node
 
 @onready var game_music: AudioStreamPlayer = $GameMusic
 
-
 func _ready() -> void:
+	Events.game_paused.connect(_on_game_paused)
+	Events.game_unpaused.connect(_on_game_unpaused)
 	set_game_music("Track 1")
 
 
 func set_game_music(track_name : StringName = "Silence") -> void:
 	game_music["parameters/switch_to_clip"] = track_name
+
+
+func _on_game_paused() -> void:
+	AudioServer.set_bus_effect_enabled(1, 1, true)
+
+
+func _on_game_unpaused() -> void:
+	AudioServer.set_bus_effect_enabled(1, 1, false)
---
+++ source/ui/audio_v_slider.gd
@@ -0,0 +1,28 @@
+class_name AudioVSlider
+extends VSlider
+
+## The name of the Audio Bus to edit
+@export var bus_name : StringName = "Master"
+var bus_idx : int = 0
+
+
+func _init() -> void:
+	min_value = 0.0
+	max_value = 1.0
+	step = 0.01
+	exp_edit = true
+
+
+func _ready() -> void:
+	bus_idx = AudioServer.get_bus_index(bus_name)
+	if bus_idx == -1:
+		self_modulate.a = 0.5
+		return
+	
+	value = AudioServer.get_bus_volume_db(db_to_linear(bus_idx))
+	value_changed.connect(_on_value_changed)
+
+
+func _on_value_changed(new_value) -> void:
+	var new_volume = linear_to_db(new_value)
+	AudioServer.set_bus_volume_db(bus_idx, new_volume)
