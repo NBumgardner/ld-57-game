71b35340351a3836fa4dc952de56da9f4c965dd5
Harmoen <harmon.the.emu@gmail.com>
2025-04-07 01:40:14 -0500

    Implemented Cheese Sound

--- source/objects/cheese/cheese.gd
+++ source/objects/cheese/cheese.gd
@@ -1,10 +1,15 @@
 extends Area2D
 
+@onready var sfx_nibble: AudioStreamPlayer2D = $"SFX|Nibble"
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered_area)
 
 
 func _on_body_entered_area(_body : Node2D) -> void:
+	set_deferred("monitoring", false)
 	Database.cheese_count += 1
+	hide()
+	sfx_nibble.play()
+	await sfx_nibble.finished
 	queue_free()
