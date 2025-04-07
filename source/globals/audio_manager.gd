11a9bc55c394f783db8999f7effe9a501f1ce8f7
Harmoen <harmon.the.emu@gmail.com>
2025-04-07 04:15:03 -0500

    Added Background Music

--- source/globals/audio_manager.gd
+++ source/globals/audio_manager.gd
@@ -1,1 +1,11 @@
 extends Node
+
+@onready var game_music: AudioStreamPlayer = $GameMusic
+
+
+func _ready() -> void:
+	set_game_music("Track 1")
+
+
+func set_game_music(track_name : StringName = "Silence") -> void:
+	game_music["parameters/switch_to_clip"] = track_name
